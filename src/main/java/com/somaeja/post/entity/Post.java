bf363d0face08c75b76305ffae21abe43fc5f27d
DESKTOP-Q7U1K2V\serrl <coffeescript@kakao.com>
2020-12-17 18:53:55 +0900

    [#16] Remove not use method

--- src/main/java/com/somaeja/post/entity/Post.java
+++ src/main/java/com/somaeja/post/entity/Post.java
@@ -40,20 +40,5 @@ public class Post {
 	public boolean isSameUserId(Long userId) {
 		return Objects.equals(this.userId, userId);
 	}
-
-	public boolean isSameLocationId(Long locationId) {
-		return Objects.equals(this.locationId, locationId);
-	}
-
-	public boolean isSameImageId(Long imageId) {
-		return Objects.equals(this.imageId, imageId);
-	}
-
-	public boolean isSameNegotiable(boolean isNegotiable) {
-		return Objects.equals(this.isNegotiable, isNegotiable);
-	}
-
-	public boolean isSameTrade(boolean isOfflineTrade) {
-		return Objects.equals(this.isOfflineTrade, isOfflineTrade);
-	}
+	
 }
