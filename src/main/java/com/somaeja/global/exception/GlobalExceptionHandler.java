93c1b58096d8d1bed95cf0d69da9f62c524c52fa
DESKTOP-Q7U1K2V\serrl <coffeescript@kakao.com>
2021-01-04 21:26:28 +0900

    [#34] Add global exception handler and method overloading of errorresponse

---
+++ src/main/java/com/somaeja/global/exception/GlobalExceptionHandler.java
@@ -0,0 +1,26 @@
+package com.somaeja.global.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+	// HttpRequestMethodNotSupportedException : 지원되지 않는 HTTP Method 가 호출되었을 경우 발생한다.
+	@ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+	protected ResponseEntity<ErrorResponse> handleNullPointerException(NullPointerException exception) {
+		final ErrorResponse response = ErrorResponse.from(exception.getMessage());
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
+	}
+
+	// 예상하지 못한 Exception Catch = 클라이언트의 White Page 를 방지하기 위함
+	@ExceptionHandler(Exception.class)
+	protected ResponseEntity<ErrorResponse> handleException() {
+		final ErrorResponse response = ErrorResponse.from();
+		return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(response);
+	}
+
+}
